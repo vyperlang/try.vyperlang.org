262c7f82b2c6079f157913818a4f3795097f3368
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-02-27 10:13:24 +0100

    Don't print env!

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # build the docker images for the project
 
-set -Eeuxo pipefail
-
 # load the environment variables
 set -o allexport; source .env; set +o allexport;
+
+set -Eeuxo pipefail
 
 docker compose build
--- full_reload.sh
+++ full_reload.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # do a full rebuild and hard restart docker compose
 
-set -Eeuxo pipefail
-
 # load the environment variables
 set -o allexport; source .env; set +o allexport;
+
+set -Eeuxo pipefail
 
 # if this is a branch, we need to discard cache
 docker compose build --no-cache
